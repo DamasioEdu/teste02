498a2d7a271ee4c508681fdc5ffb554c7396268c
DamasioEdu <edu_damasio@outlook.com>
2022-01-23 15:50:29 -0300

    teste 02

---
+++ teste02.php
@@ -0,0 +1,9 @@
+<?php 
+
+	echo "Mais uma tentativa.";
+
+			echo "Na última erramos o branch e o comando strong, tive que apagar todo o registro do branch, mas tentaremos corrigir nessa.";
+
+					echo "Tentativa de <strong>Negrito</strong>";
+
+?>
